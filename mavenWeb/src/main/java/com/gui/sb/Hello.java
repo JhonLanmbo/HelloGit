e81da92bc1b2c4c7b422b284a158c7741df31634
JhonLanmbo <66068041+JhonLanmbo@users.noreply.github.com>
2020-09-10 14:45:30 +0800

    Update Hello.java
    43132132332321

--- mavenWeb/src/main/java/com/gui/sb/Hello.java
+++ mavenWeb/src/main/java/com/gui/sb/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public void name(String name) {
 		System.out.println(name+"!!!!");
 		System.out.println(name+"!++++++!!!");
+		System.out.println(name+"!++++++!!!");
+		System.out.println(name+"!++++++!!!");
+		System.out.println(name+"!++++++!!!");
+		System.out.println(name+"!++++++!!!");
 	}
 	
 	
